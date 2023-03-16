c3661e50d14a995d060f45cf4f67ecbfc7e89e65
tomasrl18 <tomaslopezraigal@gmail.com>
2023-03-16 13:18:26 +0100

    Videos 59 - 63

--- Section 4/backend-04-update-store/pages/posts/_id/index.vue
+++ Section 4/backend-04-update-store/pages/posts/_id/index.vue
@@ -2,15 +2,12 @@
   <div class="single-post-page">
     <section class="post">
       <h1 class="post-title">{{ loadedPost.title }}</h1>
-
       <div class="post-details">
         <div class="post-detail">Last updated on {{ loadedPost.updatedDate }}</div>
         <div class="post-detail">Written by {{ loadedPost.author }}</div>
       </div>
-
       <p class="post-content">{{ loadedPost.content }}</p>
     </section>
-
     <section class="post-feedback">
       <p>Let me know what you think about the post, send a mail to <a href="mailto:feedback@my-awesome-domain.com">feedback@my-awesome-domain.com</a>.</p>
     </section>
@@ -18,11 +15,17 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from 'axios'
 
 export default {
   asyncData(context) {
-    return axios.get('https://vue-http-demo-b7ad7-default-rtdb.europe-west1.firebasedatabase.app/posts/' + context.params.id + '.json');
+    return axios.get('https://nuxt-blog.firebaseio.com/posts/' + context.params.id + '.json')
+      .then(res => {
+        return {
+          loadedPost: res.data
+        }
+      })
+      .catch(e => context.error(e))
   }
 };
 </script>
