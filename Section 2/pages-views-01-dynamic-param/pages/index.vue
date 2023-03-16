cc49395f25f22f44886ac5027b557abdf4f3a0b1
tomasrl18 <tomaslopezraigal@gmail.com>
2023-03-16 08:17:32 +0100

    Videos 15-16

--- Section 2/pages-views-01-dynamic-param/pages/index.vue
+++ Section 2/pages-views-01-dynamic-param/pages/index.vue
@@ -1,21 +1,8 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-
-      <h1 class="title">
-        nuxt-course
-      </h1>
-
-      <h2 class="subtitle">
-        Nuxt.js course for Udemy
-      </h2>
-      
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <nuxt-link to="/users">
+      Users
+    </nuxt-link>
   </section>
 </template>
 
