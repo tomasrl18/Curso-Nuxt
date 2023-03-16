d651a28c978d4dfdf8d8a3f3975834ab286302e0
tomasrl18 <tomaslopezraigal@gmail.com>
2023-03-16 13:54:49 +0100

    Videos 64 - 76

---
+++ Section 6/config-plugins-modules-03-modules/nuxt.config.js
@@ -0,0 +1,75 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'WD Blog',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'My cool Web Development Blog' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css?family=Open+Sans" }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fa923f', height: '4px', duration: 5000 },
+  loadingIndicator: {
+    name: 'circle',
+    color: '#fa923f'
+  },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    '~assets/styles/main.css'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '~plugins/core-components.js',
+    '~plugins/date-filter.js'
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/axios',
+  ],
+  axios: {
+    baseURL: process.env.BASE_URL || 'https://vue-http-demo-b7ad7-default-rtdb.europe-west1.firebasedatabase.app',
+    credentials: false
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+
+    }
+  },
+  env: {
+    baseUrl: process.env.BASE_URL || 'https://vue-http-demo-b7ad7-default-rtdb.europe-west1.firebasedatabase.app'
+  },
+  transition: {
+    name: 'fade',
+    mode: 'out-in'
+  }
+}
